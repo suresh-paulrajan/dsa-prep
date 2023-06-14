45a124ba6bdafc548d62492daae7010960f38919
Suresh Paulrajan <sureshpaulrajan@gmail.com>
2023-06-14 09:46:51 +0530

    LeetCode - Explore - Arrays101

---
+++ src/main/java/leetcode/arrays101/SearchForDouble.java
@@ -0,0 +1,27 @@
+package main.java.leetcode.arrays101;
+
+import main.java.utils.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SearchForDouble {
+    static Logger log = new Logger();
+    public static void main(String[] args) {
+//        int[] input = new int[] {10,2,5,3};
+//        int[] input = new int[] {3,1,7,11};
+        int[] input = new int[] {-2, 0, 10, -19, 4, 6, -8};
+        log.info(checkIfExist(input));
+    }
+
+    private static boolean checkIfExist(int[] arr) {
+        Map<Integer, String> store = new HashMap<>();
+        for (int num : arr) {
+            if(store.containsKey(num*2) || ((num % 2 == 0) && (store.containsKey(num/2)))) {
+                return true;
+            }
+            store.put(num, "");
+        }
+        return false;
+    }
+}
